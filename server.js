acfd54e1d1cdafb2ccb8c7bb740ec126a8d6956c
Kashish Kataria <Kashish.Kataria@evryindia.in>
2025-06-07 10:03:31 +0530

    Change 3

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectDatabase();
 
 app.use('/api/v1',ExpenseRoutes);
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
     console.log(`Server Started at PORT ${PORT}`.bgBlue);
